6a6bb78ccea70b5ccc79bcd83c8ad93ce9f706f8
yu_h-neu <yu_h-neu@reachauto.com>
2022-01-08 12:06:31 +0800

    impl T for Fn

--- grpc2/examples/server.rs
+++ grpc2/examples/server.rs
@@ -110,7 +110,7 @@ impl RouteGuide for RouteGuideService {
             }
         };
 
-        Ok(Response::new(Box::pin(output) as Self::RouteChatStream))
+        Ok(Response::new(Box::pin(output)))
     }
 }
 
